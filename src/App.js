7941e1ff46b74b70f1c46db5f6476ab2b462db1a
Kate-Vol <vahrusheva_katya@bk.ru>
2023-04-04 18:22:38 +0300

    Taking out PostForm in separate components. Use callback function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './Styles/App.css'
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
 
 function App () {
     const [posts, setPosts] = useState([
@@ -11,34 +12,13 @@ function App () {
         {id: 3, title: 'JavaScript 3', body: 'Despription'}
     ])
 
-    const[post, setPost] = useState({title: '', body: ''})
-
-    const addNewPost = (e) => {
-         e.preventDefault()
-        setPosts([...posts, {...post, id: Date.now()}])
-        setPost({title: '', body: ''})
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
     }
 
   return (
     <div className="App">
-        <form>
-            <MyInput
-                value={post.title}
-                onChange={e => setPost({...post, title: e.target.value})}
-                type="text"
-                placeholder='Post Name'
-            />
-
-
-            <MyInput
-                value={post.body}
-                onChange={e => setPost({...post, body: e.target.value})}
-                type='text'
-                placeholder='Post description'
-
-            />
-            <MyButton onClick={addNewPost}>New post</MyButton>
-        </form>
+        <PostForm create={createPost}/>
         <PostList posts={posts} title={'Posts list 1'}/>
     </div>
   );
--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
+import MyInput from "./UI/input/MyInput";
+import MyButton from "./UI/button/MyButton";
 
-const PostForm = () => {
+const PostForm = ({create}) => {
+
+    const[post, setPost] = useState({title: '', body: ''})
+
+    const addNewPost = (e) => {
+        e.preventDefault()
+        const newPost = {
+            ...post, id: Date.now()
+        }
+        create(newPost)
+        setPost({title: '', body: ''})
+    }
+
     return (
-        <div>
-            
-        </div>
+        <form>
+            <MyInput
+                value={post.title}
+                onChange={e => setPost({...post, title: e.target.value})}
+                type="text"
+                placeholder='Post Name'
+            />
+
+
+            <MyInput
+                value={post.body}
+                onChange={e => setPost({...post, body: e.target.value})}
+                type='text'
+                placeholder='Post description'
+
+            />
+            <MyButton onClick={addNewPost}>New post</MyButton>
+        </form>
     );
 };
 
