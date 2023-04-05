9feca35bba96b8088d1c5cf1da47b53b8efe48ed
Kate-Vol <vahrusheva_katya@bk.ru>
2023-04-05 12:11:32 +0300

    EDU-377- Create own hooks with sorted and searched logics

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
+import {usePosts} from "./components/hooks/usePosts";
 
 function App () {
     const [posts, setPosts] = useState([
@@ -17,19 +18,7 @@ function App () {
 
     const[filter, setFilter] = useState({sort:'', query: ''})
     const[modal, setModal] = useState(false)
-
-
-    const sortedPosts = useMemo( () => {
-        console.log('getSortedPosts is worked')
-        if (filter.sort) {
-            return [...posts].sort((a,b) => a[filter.sort].localeCompare(b[filter.sort]))
-        }
-        return posts;
-    }, [filter.sort, posts])
-
-    const sortedAndSeacheredPosts = useMemo( () => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-    }, [filter.query, sortedPosts])
+    const sortedAndSearchedPosts= usePosts(posts, filter.sort, filter.query)
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -57,7 +46,7 @@ function App () {
             filter ={filter}
             setFilter={setFilter}
         />
-                <PostList remove={removePost} posts={sortedAndSeacheredPosts} title={'Posts list 1'}/>
+                <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Posts list 1'}/>
     </div>
   );
 }
