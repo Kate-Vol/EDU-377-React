8222f861dde6aa3991dd266493c76d5baa08966b
Kate-Vol <vahrusheva_katya@bk.ru>
2023-04-05 17:20:54 +0300

    EDU-377- Add routing and navbar

--- src/App.js
+++ src/App.js
@@ -1,93 +1,17 @@
-import React, {useEffect, useMemo, useRef, useState} from "react";
+import React from "react";
 import './styles/App.css'
-import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
-import PostForm from "./components/PostForm";
-import MySelect from "./components/UI/select/MySelect";
-import PostFilter from "./components/PostFilter";
-import MyModal from "./components/UI/MyModal/MyModal";
-import {usePosts} from "./components/hooks/usePosts";
-import axios from "axios";
-import PostService from "./API/PostService";
-import Loader from "./components/UI/Loader/Loader";
-import {useFetching} from "./components/hooks/useFetching";
-import {getPageCount, getPagesArray} from "./utils/pages";
-import Pagination from "./components/UI/pagination/Pagination";
+import {BrowserRouter} from "react-router-dom";
+import Navbar from "./components/UI/navbar/Navbar";
+import AppRouter from "./components/AppRouter";
 
 function App () {
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'aaaJavaScript', body: 'sssDespription'},
-        {id: 2, title: 'bbbJavaScript 2', body: 'dddDespription'},
-        {id: 3, title: 'JavaScript 3', body: 'aaaDespription'}
-    ])
+    return (
+        <BrowserRouter>
+            <Navbar/>
+            <AppRouter/>
 
-    const [filter, setFilter] = useState({sort:'', query: ''})
-    const [modal, setModal] = useState(false)
-    const [totalPages, setTotalPages] = useState(0)
-    const [limit, setLimit] = useState(10)
-    const [page, setPage] = useState(1)
-    const sortedAndSearchedPosts= usePosts(posts, filter.sort, filter.query)
-
-    const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
-        const  response= await PostService.getAll(limit, page);
-        setPosts(response.data)
-        const totalCount = response.headers['x-total-count']
-        setTotalPages(getPageCount(totalCount, limit))
-    })
-
-    console.log(totalPages)
-    useEffect(() => {
-        fetchPosts(limit, page)
-    }, [])
-
-    const createPost = (newPost) => {
-        setPosts([...posts, newPost])
-        setModal(false)
-    }
-
-
-    //Получаем post из дочернего компонента
-    const removePost = (post)=>{
-        setPosts(posts.filter(p => p.id !== post.id))
-    }
-
-    const changePage = (page) => {
-        setPage(page)
-        fetchPosts(limit, page)
-    }
-
-
-
-  return (
-    <div className="App">
-        <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
-            Create users
-        </MyButton>
-        <MyModal visible={modal} setVisible={setModal}>
-            <PostForm create={createPost}/>
-        </MyModal>
-
-        <hr style={{margin: "15px 0"}}/>
-        <PostFilter
-            filter ={filter}
-            setFilter={setFilter}
-        />
-        {postError &&
-            <h1>It's error ${postError}</h1>
-        }
-        {isPostsLoading
-            ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
-            : <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Posts list 1'}/>
-        }
-        <Pagination
-            page={page}
-            changePage={changePage}
-            totalPages={totalPages}
-        />
-
-    </div>
-  );
+        </BrowserRouter>
+    )
 }
 
 export default App;
---
+++ src/pages/Error.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <h1 style={{color: 'red'}}>
+                There is no such page
+            </h1>
+        </div>
+    );
+};
+
+export default Error;
