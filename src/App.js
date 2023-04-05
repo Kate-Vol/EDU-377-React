9811894ec3b0d7ee96dbb090832e856661b7ca78
Kate-Vol <vahrusheva_katya@bk.ru>
2023-04-05 15:53:23 +0300

    EDU-377- Add Pagination

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useMemo, useRef, useState} from "react";
-import './Styles/App.css'
+import './styles/App.css'
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
@@ -12,6 +12,8 @@ import axios from "axios";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
 import {useFetching} from "./components/hooks/useFetching";
+import {getPageCount, getPagesArray} from "./utils/pages";
+import Pagination from "./components/UI/pagination/Pagination";
 
 function App () {
     const [posts, setPosts] = useState([
@@ -22,14 +24,21 @@ function App () {
 
     const [filter, setFilter] = useState({sort:'', query: ''})
     const [modal, setModal] = useState(false)
+    const [totalPages, setTotalPages] = useState(0)
+    const [limit, setLimit] = useState(10)
+    const [page, setPage] = useState(1)
     const sortedAndSearchedPosts= usePosts(posts, filter.sort, filter.query)
-    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-        const posts = await PostService.getAll();
-        setPosts(posts)
+
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
+        const  response= await PostService.getAll(limit, page);
+        setPosts(response.data)
+        const totalCount = response.headers['x-total-count']
+        setTotalPages(getPageCount(totalCount, limit))
     })
 
+    console.log(totalPages)
     useEffect(() => {
-        fetchPosts()
+        fetchPosts(limit, page)
     }, [])
 
     const createPost = (newPost) => {
@@ -41,6 +50,11 @@ function App () {
     //Получаем post из дочернего компонента
     const removePost = (post)=>{
         setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    const changePage = (page) => {
+        setPage(page)
+        fetchPosts(limit, page)
     }
 
 
@@ -66,6 +80,12 @@ function App () {
             ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
             : <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Posts list 1'}/>
         }
+        <Pagination
+            page={page}
+            changePage={changePage}
+            totalPages={totalPages}
+        />
+
     </div>
   );
 }
