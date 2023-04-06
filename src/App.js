9a3d0ea3c0126e02acc49e3c61003c1cd21c1da9
Kate-Vol <vahrusheva_katya@bk.ru>
2023-04-06 11:52:29 +0300

    EDU-377- 3. Deploy react project in Github

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import AppRouter from "./components/AppRouter";
 
 function App () {
     return (
-        <BrowserRouter  basename={process.env.PUBLIC_URL}>
+        <BrowserRouter>
             <Navbar/>
             <AppRouter/>
 
